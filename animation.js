0ec9a2d7fdf756451957cb6bb48aae797487537f
Loida Jane Anfone <71513001+its-el-jey@users.noreply.github.com>
2021-01-22 12:11:04 +0800

    animations and some adjustments

---
+++ animation.js
@@ -0,0 +1,19 @@
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+  });
+  
+  
+  sr.reveal('.site-logo', {}); 
+  sr.reveal('.t-hero', {delay: 100}); 
+  sr.reveal('.t-hero-slogan', {delay: 200}); 
+  sr.reveal('.buttons', {delay: 300}); 
+  sr.reveal('.pros-image', {delay: 300}); 
+  sr.reveal('.t-section-title', {delay: 200}); 
+  sr.reveal('.t-section-slogan', {delay: 300}); 
+  sr.reveal('.anywhere-image', {delay: 300}); 
+  sr.reveal('.pros-group-copy', {delay: 300}); 
+  sr.reveal('.work-flow-item', {delay: 400}); 
